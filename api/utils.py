e37236819fa59ae9c6dbb24a18eeee9c66d397b0
slojar <52827399+slojar@users.noreply.github.com>
2022-04-28 11:32:12 +0100

    updateProfileUpdate

--- api/utils.py
+++ api/utils.py
@@ -24,35 +24,58 @@ def get_user_detail(uid, access_token):
 
 
 def update_user_profile(response, user):
-    user.first_name = response['first_name']
-    user.last_name = response['last_name']
-    user.email = response['email']
+    if response['first_name']:
+        user.first_name = response['first_name']
+    if response['last_name']:
+        user.last_name = response['last_name']
+    if response['email']:
+        user.email = response['email']
     user.save()
 
     profile, _ = Profile.objects.get_or_create(user=user)
-    profile.uid = response['id']
-    profile.dob = response['birthday']
-    profile.gender = response['gender']
-    profile.middle_name = response['middle_name']
-    profile.hometown = response['hometown']['name']
-    profile.profile_picture = response['picture']['data']['url']
-    profile.location = response['location']['name']
+    if response['id']:
+        profile.uid = response['id']
+    if response['birthday']:
+        profile.dob = response['birthday']
+    if response['gender']:
+        profile.gender = response['gender']
+    if response['middle_name']:
+        profile.middle_name = response['middle_name']
+    if response['hometown']['name']:
+        profile.hometown = response['hometown']['name']
+    if response['picture']['data']['url']:
+        profile.profile_picture = response['picture']['data']['url']
+    if response['location']['name']:
+        profile.location = response['location']['name']
     profile.save()
 
-    account = response['accounts']['data']
+    account = []
+    if response['accounts']['data']:
+        account = response['accounts']['data']
     for data in account:
         page, _ = Page.objects.get_or_create(profile=profile, page_uid=data['id'])
-        page.name = data['name']
-        page.about = data['about']
-        page.page_token = data['access_token']
-        page.page_picture = data['picture']['data']['url']
-        page.category = data['category']
-        page.followers = data['followers_count']
-        page.fans = data['fan_count']
-        page.likes = data['new_like_count']
-        page.has_whatsapp_number = data['has_whatsapp_number']
-        page.whatsapp_number = data['whatsapp_number']
-        page.page_link = data['link']
+        if data['name']:
+            page.name = data['name']
+        if data['about']:
+            page.about = data['about']
+        if data['access_token']:
+            page.page_token = data['access_token']
+        if data['picture']['data']['url']:
+            page.page_picture = data['picture']['data']['url']
+        if data['category']:
+            page.category = data['category']
+        if data['followers_count']:
+            page.followers = data['followers_count']
+        if data['fan_count']:
+            page.fans = data['fan_count']
+        if data['new_like_count']:
+            page.likes = data['new_like_count']
+        if data['has_whatsapp_number']:
+            page.has_whatsapp_number = data['has_whatsapp_number']
+        if data['whatsapp_number']:
+            page.whatsapp_number = data['whatsapp_number']
+        if data['link']:
+            page.page_link = data['link']
         page.save()
 
     return True
